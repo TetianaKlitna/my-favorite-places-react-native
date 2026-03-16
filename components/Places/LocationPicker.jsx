b07cb2e900f1ef79cfd2b9f0e00cd451347b2d9b
Tetiana Klitna <tetiana@artcraft.studio>
2026-03-15 23:03:16 -0700

    added Location Picker

--- components/Places/LocationPicker.jsx
+++ components/Places/LocationPicker.jsx
@@ -1,9 +1,50 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location";
 
 function LocationPicker() {
+
+    const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+    const verifyPermissions = async () => {
+        if (locationPermissionInformation.status === PermissionStatus.GRANTED) {
+            const permissionResponse = await requestPermission();
+            return permissionResponse.granted;
+        }
+        if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+            return false;
+        }
+        const permissionResponse = await requestPermission();
+        return permissionResponse.granted;
+    };
+
+    const getLocationHandler = async () => {
+        const hasPermission = await verifyPermissions();
+        if (!hasPermission) {
+            return;
+        }
+        const location = await getCurrentPositionAsync();
+        console.log(location);
+    };
+
+    const pickOnMapHandler = () => {
+        // Open a map to let the user pick a location
+    };
+
     return (
         <View>
-            <Text>Location Picker</Text>
+            <View style={styles.mapPreview}>
+                <Text>Location Picker</Text>
+            </View>
+            <View style={styles.actions}>
+                <OutlinedButton icon="location" size={24} color={Colors.primary500} onPress={getLocationHandler}>
+                    Locate User
+                </OutlinedButton>
+                <OutlinedButton icon="map" size={24} color={Colors.primary500} onPress={pickOnMapHandler}>
+                    Pick on Map
+                </OutlinedButton>
+            </View>
         </View>
     );
 }   
@@ -11,5 +52,20 @@ function LocationPicker() {
 export default LocationPicker;
 
 const styles = StyleSheet.create({
-    // Add your styles here
+    mapPreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+        overflow: 'hidden',
+    },  
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    
 });
--- components/Places/PlaceForm.jsx
+++ components/Places/PlaceForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { ScrollView, View, Text, TextInput, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
 
 function PlaceForm() {
 
@@ -18,6 +19,7 @@ function PlaceForm() {
         />
       </View>
       <ImagePicker />
+      <LocationPicker />
     </ScrollView>
   );
 }
